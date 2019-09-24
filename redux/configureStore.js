0505dad3011470f3c0722ccc82a91ffd36f5f6fd
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-24 18:33:58 +0800

    Create redux store
    - combine currentUser and erorr reducers in a rootReducer
    - create redux store

---
+++ redux/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const composeEnhancers = composeWithDevTools({
+  trace: true,
+  traceLimit: 25,
+});
+
+const middleWares = process.env.NODE_ENV === 'development' ? composeEnhancers(applyMiddleware(thunk, logger)) : compose(applyMiddleware(thunk));
+
+export default (initialState = {}) => createStore(
+  rootReducer,
+  initialState,
+  middleWares,
+);
