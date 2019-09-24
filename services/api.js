d8d00aa6178d7b0147e7bd565efa4e26bcd75351
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-09-24 18:40:37 +0800

    Create base API request sending functions
    1. In services folder create api.js file
    2. Create sendRequest function as generic module for sending requests to
    backend API
    3. Create setAuthorizationToken function to provide token header to each
    API request

---
+++ services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const sendRequest = async (method, path, data) => {
+  const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.API_URL;
+  const result = await axios[method](`${baseUrl}/${path}`);
+  return result;
+};
+
+export const setAuthorizationToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
